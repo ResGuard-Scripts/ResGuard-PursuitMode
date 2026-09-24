098b340334da25e0c45bbd679f1b8ec7e81f4c08
Bruneky <miskomuki920@gmail.com>
2026-09-24 22:21:28 +0200

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'ResGuard | Bruneky'
 description 'ResGuard PursuitMode - Vehicle Performance & Visual Effects'
-version '1.1.0'
+version '1.1.1'
 
 shared_scripts {
     'config.lua',
